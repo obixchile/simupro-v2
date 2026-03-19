6ea65e9df166d86af90e288bddbaccc4bf8d587d
obixchile <obixchile@gmail.com>
2026-03-19 20:06:18 -0300

    add src/pages/NuevaPropuesta.tsx

---
+++ src/pages/NuevaPropuesta.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function NuevaPropuesta(){
+  return <div className='p-6'><h1 className='text-2xl font-bold'>NuevaPropuesta</h1><p className='text-gray-500 mt-2'>Seccion en construccion</p></div>;
+}
